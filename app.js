bbfb2c271f568e6813e024720a48d91d8bdd6679
Amitkr5 <110593142+Amitkr5@users.noreply.github.com>
2022-08-23 11:54:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'constant-Type': 'text/html' })
+    fs.readFile('app.js', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+
+
+})
+
+server.listen(port, function(error) {
+    if (error) {
+        console.log('something went wrong', error)
+    } else {
+        console.log('server is listening on port ' + port)
+
+    }
+})
